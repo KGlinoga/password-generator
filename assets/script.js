fc4d0e48c043911151ae62140953bf7ed3a606b6
Kristina Faye <kglinoga@gmail.com>
2022-06-23 19:40:00 -0700

    filled JS file in with basic notes from Office Hours rundown. Does contain unfamiliar concepts but pretty sure those will be covered in Friday's lecture.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
+var length;
+var availableCharacters = "";
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +16,35 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword () {
+  var password = ""
+  // 1. prompt user for parameters
+  // length, special chars, numbers, uppercase, lowercase
+  var parameters = getParameters();
+
+  // 2. create random from password from available characters
+  // for loop run the number of times that user sets length to
+  // add random character to password string
+  for (i=0; i<parameters.length; i++) {
+    // add 1 random character to password
+  }
+
+
+  return password
+}
+
+
+function getParameters() {
+    // set lenght via prompt
+    // var parameters = {
+    //   length: 0,
+    //   availableChars: "1234567890abcdefghijklmnop" **OR do we do random letters?? lol I couldn't really tell from the demo
+ //   }
+  lenth = prompt()
+  if (length<8) {
+
+  }
+    // add characters to availableCharacters via user input
+
+    //return parameters
+}
