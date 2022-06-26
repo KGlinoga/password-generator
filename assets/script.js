d2d4f429ae4b3d780f8f338a7ae402a9e6c2e12f
Kristina Faye <kglinoga@gmail.com>
2022-06-26 13:22:02 -0700

    post-tutor session save - has very advanced techniques, and is quite elegant, but will likely trip the plagiarism bots lol.  b/c no, we did not really cover these thingsin class yet.

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,47 @@
-var length;
-var availableCharacters = "";
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+console.log("hello")
+console.log(generateBtn)
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 // Write password to the #password input
 function writePassword() {
+  console.debug("testing write password")
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordTextEl = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordTextEl.value = password;
 
 }
 
-function generatePassword () {
-  var password = ""
-  // 1. prompt user for parameters
-  // length, special chars, numbers, uppercase, lowercase
-  var parameters = getParameters();
+function generatePassword() {
 
-  // 2. create random from password from available characters
-  // for loop run the number of times that user sets length to
-  // add random character to password string
-  for (i=0; i<parameters.length; i++) {
-    // add 1 random character to password
+  const params = getParameters();
+  if (!params) return "";
+
+  const nums = "0123456789";
+  const lowerCase = "abcdefghijklmnopqrstuvxyz";
+  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+  const specialChars = "!@#$%^&*()";
+  let setOfChars = "";
+  let password = "";
+
+  if (params.hasLowerCaseChar) setOfChars += lowerCase;
+  if (params.hasUpperCaseChar) setOfChars += upperCase;
+  if (params.hasNumericChar) setOfChars += nums;
+  if (params.hasSpecialChar) setOfChars += specialChars;
+
+  for (let i = 0; i < params.length; i++) {
+    
+    // generate random number between 0 and length of setOfChars
+    const randomIndex = Math.floor(Math.random()*setOfChars.length);    
+    password += setOfChars[randomIndex]
   }
+
+
 
 
   return password
@@ -35,16 +49,24 @@ function generatePassword () {
 
 
 function getParameters() {
-    // set lenght via prompt
-    // var parameters = {
-    //   length: 0,
-    //   availableChars: "1234567890abcdefghijklmnop" **OR do we do random letters?? lol I couldn't really tell from the demo
- //   }
-  lenth = prompt()
-  if (length<8) {
 
+
+  const inputString = prompt("Enter the length of the password");
+  const length = parseInt(inputString);
+  if (Number.isNaN(length) || length < 8 || length > 128) {
+    alert("Length should be a number between 7 and 129.");
+    return null;
   }
-    // add characters to availableCharacters via user input
+  // add characters to availableCharacters via user input
+  const hasSpecialChar = confirm("Do you want special characters?")
+  const hasNumericChar = confirm("Do you want numeric characters?")
+  const hasUpperCaseChar = confirm("Do you want uppercase characters?")
+  const hasLowerCaseChar = confirm("Do you want lowercase characters?")
 
-    //return parameters
+  if (!hasSpecialChar && !hasNumericChar && !hasUpperCaseChar && !hasLowerCaseChar) {
+    alert("Password needs at least one option selected");
+    return null;
+  }
+  return { length, hasSpecialChar, hasNumericChar, hasUpperCaseChar, hasLowerCaseChar };
+
 }
